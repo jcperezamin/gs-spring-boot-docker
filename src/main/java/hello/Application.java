46cd0f019dc5c3667a85748ad34692c30822f044
Juan Carlos Perez Amin <juancarlosperezamin@yahoo.com>
2018-03-14 16:34:24 -0500

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Demo Microservicios con 3 zonas de disponibilidad";
+        return "Universidad de Cartagena";
     }
 
     public static void main(String[] args) {
