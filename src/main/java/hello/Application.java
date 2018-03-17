9563a369edb3c5e58eb87e2b94c9ebd974755d29
Juan Carlos Perez Amin <juancarlosperezamin@yahoo.com>
2018-03-16 19:56:15 -0500

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Universidad de Cartagena";
+        return "Tecnar";
     }
 
     public static void main(String[] args) {
